576df0281c52b86607469383aa6a6c974772f22c
Brendan Woo <woo.brendan@gmail.com>
2023-10-12 15:50:18 -0700

    create variable key_list with keys to keep from original csv file. create function clean keys

--- csv_converter.py
+++ csv_converter.py
@@ -3,6 +3,18 @@ import io
 
 file_path = "./RA_entries.csv"
 
+key_list = [
+    'Car Class',
+    'Driver Designation',
+    'Registered Car #',
+    'Team Name',
+    'Driver Name (First Name)',
+    'Driver Name (Last Name)',
+    '2nd Driver (First Name)',
+    '2nd Driver (Last Name)',
+    'Event Selection'
+]
+
 
 def csv_to_dict_arr(csv_file):
 
@@ -14,6 +26,18 @@ def csv_to_dict_arr(csv_file):
             entry_dict = dict(zip(headers, row))
             entries.append(entry_dict)
     return entries
+
+
+def clean_results(arr, key_arr):
+    entries = []
+    for entry in arr:
+        filtered = {key: entry[key] for key in key_arr if key in entry}
+        entries.append(filtered)
+    return entries
+
+
+def csv_to_clean_keys(csv_file):
+    dict_arr = csv_to_dict_arr(csv_file)
 
 
 print(csv_to_dict_arr(file_path))
