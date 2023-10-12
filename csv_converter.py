cd91dc043077d7d2771c0bdb99273452cdc8eaea
Brendan Woo <woo.brendan@gmail.com>
2023-10-12 15:36:54 -0700

    update to open csv file and read through as csv_file

--- csv_converter.py
+++ csv_converter.py
@@ -5,10 +5,15 @@ file_path = "./RA_entries.csv"
 
 
 def csv_to_dict_arr(csv_file):
-    csv_reader = csv.reader(io.TextIOWrapper(csv_file, encoding='utf-8'))
-    headers = next(csv_reader)
-    entries = []
-    for row in csv_reader:
-        entry_dict = dict(zip(headers, row))
-        entries.append(entry_dict)
+
+    with open(file_path, 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        headers = next(csv_reader)
+        entries = []
+        for row in csv_reader:
+            entry_dict = dict(zip(headers, row))
+            entries.append(entry_dict)
     return entries
+
+
+print(csv_to_dict_arr(file_path))
