48a64ab11f7c9535c0bdceb8105cd430587f832a
Brendan Woo <woo.brendan@gmail.com>
2023-10-13 14:54:34 -0700

    import getWCentries, and csv file path. inside fill excel template, create variables for event, series. create save path and file name

--- useTemplate.py
+++ useTemplate.py
@@ -1,6 +1,8 @@
 import openpyxl
+from .csv_converter import getWCEntries
 
 template = './driver_template.xltx'
+csv_file = './RA_entries.csv'
 
 # {'Driver Designation': 'Pro - Am', 'Team Name': 'RealTime', 'number': '43', 'event': 'Road America', 'series': 'GTWCA', 'driver2': 'Adam Christodoulou', 'driver1': 'Anthony Bartone'}
 
@@ -16,9 +18,11 @@ header_mapping = {
 # data ==  array or dict
 
 
-def fill_excel_template(data, template_path, output_path):
+def fill_excel_template(data, template_path):
     wb = openpyxl.load_workbook(template)
     sheet = wb.active
+    event = data[0]['event']
+    series = data[0]['series']
 
     for row_i, row_data in enumerate(data, start=2):
         for col_i, key in enumerate(row_data.keys(), start=1):
@@ -29,4 +33,7 @@ def fill_excel_template(data, template_path, output_path):
                 # Get the column index for the mapped header
                 col_i = sheet[1].index(mapped_header) + 1
                 sheet.cell(row=row_i, column=col_i).value = row_data[key]
-    wb.save()
+    wb.save(f'./Driver_Sign_in/{series}_{event}.xlsx')
+
+
+fill_excel_template(getWCEntries(csv_file), template)
