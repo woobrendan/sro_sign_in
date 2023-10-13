90c3d983b46a95d5777341c877b0672b79b1ba71
Brendan Woo <woo.brendan@gmail.com>
2023-10-13 15:41:04 -0700

    comment out print statement, and move driver declaration in change key name to be before driver2

--- csv_converter.py
+++ csv_converter.py
@@ -65,6 +65,10 @@ def change_key_name(dict_arr):
             entry['series'] = getSeries(entry['Car Class'])
             del entry['Car Class']
 
+        driver1 = f"{entry.pop('Driver Name (First Name)', '')} {entry.pop('Driver Name (Last Name)', '')}".strip(
+        )
+        entry['driver1'] = driver1
+
         if '2nd Driver (First Name)' in entry and entry['2nd Driver (First Name)']:
             driver2 = f"{entry.pop('2nd Driver (First Name)', '')} {entry.pop('2nd Driver (Last Name)', '')}".strip(
             )
@@ -73,9 +77,6 @@ def change_key_name(dict_arr):
             entry.pop('2nd Driver (First Name)')
             entry.pop('2nd Driver (Last Name)')
 
-        driver1 = f"{entry.pop('Driver Name (First Name)', '')} {entry.pop('Driver Name (Last Name)', '')}".strip(
-        )
-        entry['driver1'] = driver1
     return dict_arr
 
 
@@ -106,4 +107,4 @@ def getWCEntries(csv_file):
     return sorted_series['GTWCA']
 
 
-print(getWCEntries(file_path))
+# print(getWCEntries(file_path))
