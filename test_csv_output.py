4b3070aa28535607d125efbce23af9d4833f5c4c
Brendan Woo <woo.brendan@gmail.com>
2023-10-18 16:58:23 -0400

    add in csv file that fetched all 23 entries. pass param event name to csv to series entries. add in variable to get gtwca entries

--- test_csv_output.py
+++ test_csv_output.py
@@ -2,16 +2,17 @@ import pandas as pd
 from csv_to_series_entries import csv_to_series_entries
 
 
-# csv_file = './entries_23.csv'
-csv_file = './RA_entries.csv'
-event = 'Sonoma Raceway'
+csv_file = './entries_23.csv'
+# csv_file = './RA_entries.csv'
+event_name = 'Sonoma Raceway'
 
 
-def output_csv(csv_file, event):
-    entries = csv_to_series_entries(csv_file)
+def output_csv(csv_file):
+    entries = csv_to_series_entries(csv_file, event_name)
+    series_entries = entries['GTWCA']
     excel_file = 'output.xlsx'
 
-    data = pd.DataFrame(entries)
+    data = pd.DataFrame(series_entries)
     data.to_excel(excel_file, index=False)
 
 
