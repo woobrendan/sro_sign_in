d7bff00ad6172ee6ded7616c7242ab198aad4879
Brendan Woo <woo.brendan@gmail.com>
2023-10-25 15:55:27 -0400

    create func to get all teams in an array

--- functions/utility.py
+++ functions/utility.py
@@ -5,3 +5,17 @@ def getSeries(series_name):
         'GTA': 'GTAM'
     }
     return series[series_name]
+
+# take in series_entries {'gtwca': [{entry}]} and return combined list of all entries removed duplicates
+
+
+def get_all_teams(series_entries):
+    all_entries = []
+
+    for entries in series_entries.values():
+        for entry in entries:
+            all_entries.append(entry['Team Name'].strip())
+
+    unique_teams = list(set(all_entries))
+
+    return sorted(unique_teams)
