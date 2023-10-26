9a72d6e261888155a3f9701cf423fde353bc2deb
Brendan Woo <woo.brendan@gmail.com>
2023-10-26 16:16:30 -0400

    move event log func into excel_handlers file

--- functions/utility.py
+++ functions/utility.py
@@ -19,19 +19,3 @@ def get_all_teams(series_entries):
     unique_teams = list(set(all_entries))
 
     return sorted(unique_teams)
-
-
-def event_log(wb, series_entries):
-    # Updates event logistics page of signin
-    sheet = wb['Event_log']
-
-    all_entries = get_all_teams(series_entries)
-
-    current = 8
-
-    for team_name in all_entries:
-        sheet.cell(row=current, column=1, value=team_name)
-
-        current += 1
-
-    return wb
