e4764a8b8f421f200f530ef6d617468d74c93130
Brendan Woo <woo.brendan@gmail.com>
2023-10-27 14:47:00 -0400

    remove header mapping, and remove enumerate loops in place for single loop that use hardcoded columns to target values and a current row counter

--- functions/create_drivers_signin.py
+++ functions/create_drivers_signin.py
@@ -3,34 +3,23 @@ from functions.csv_to_series_entries import csv_to_series_entries
 
 # {'Driver Designation': 'Pro - Am', 'Team Name': 'RealTime', 'number': '43', 'event': 'Road America', 'series': 'GTWCA', 'driver2': 'Adam Christodoulou', 'driver1': 'Anthony Bartone'}
 
-header_mapping = {
-    'number': 'Car #',
-    'Team Name': 'Team',
-    'driver1': 'Driver 1',
-    'driver2': 'Driver 2',
-    'Signature': 'Signature',
-    'Signature2': 'Signature2'
-}
-
 
 def create_drivers_signin(series_entries, event):
     wb = openpyxl.load_workbook('./templates/driver_master.xlsx')
 
-    # Loop through each series key and go through entries to complete data
     for series, entries in series_entries.items():
         sheet = wb[series]
 
-        for row_i, entry in enumerate(entries, start=8):
-            for col_i, key in enumerate(entry.keys(), start=1):
+        current = 8
 
-                if key in header_mapping:
+        for entry in entries:
+            sheet.cell(row=current, column=1, value=entry['number'])
+            sheet.cell(row=current, column=2, value=entry['Team Name'])
+            sheet.cell(row=current, column=3, value=entry['driver1'])
 
-                    mapped_header = header_mapping[key]
-                    # Find the column index for the mapped header
-                    for col in range(1, sheet.max_column + 1):
-                        if sheet.cell(row=7, column=col).value == mapped_header:
-                            col_i = col
-                            break
+            if series == 'GTWCA' or series == 'PGT4A':
+                sheet.cell(row=current, column=5, value=entry['driver2'])
 
-                    sheet.cell(row=row_i, column=col_i).value = entry[key]
+            current += 1
+
     wb.save(f'./Driver_Sign_in/{event}.xlsx')
