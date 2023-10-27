ca94d5d8252a4abd0f913646d05b52df50582d48
Brendan Woo <woo.brendan@gmail.com>
2023-10-27 17:12:05 -0400

    inside change key name change driver designation to be classif

--- functions/csv_to_series_entries.py
+++ functions/csv_to_series_entries.py
@@ -76,6 +76,7 @@ def change_key_name(dict_arr, event):
         driver1 = f"{entry.pop('Driver Name (First Name)', '')} {entry.pop('Driver Name (Last Name)', '')}".strip(
         )
         entry['driver1'] = driver1
+        entry['classif'] = entry.pop('Driver Designation')
 
         if '2nd Driver (First Name)' in entry and entry['2nd Driver (First Name)']:
             driver2 = f"{entry.pop('2nd Driver (First Name)', '')} {entry.pop('2nd Driver (Last Name)', '')}".strip(
