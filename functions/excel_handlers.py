2374f944cd408a1b7a6278e23ad26d85e8429639
Brendan Woo <woo.brendan@gmail.com>
2023-10-27 17:11:40 -0400

    complete loop logic to fill all columns. add variables for event and date, and pass those to the title, targeting sheet cell directly outside of loop

--- functions/excel_handlers.py
+++ functions/excel_handlers.py
@@ -22,16 +22,31 @@ def event_log(wb, series_entries):
 
 
 def handle_single_driver(wb, series_entries):
-    series = series_entries[0]['series']
-    series_name = series_long_name(series)
+    first_entry = series_entries[0]
+    series = first_entry['series']
 
-    sheet = wb['series']
+    series_name = series_long_name(first_entry['series'])
+
+    sheet = wb[series]
 
     current = 7
+
+    # Entry list title, event name and date
+    event_name = first_entry['event']
+
+    # Change date value accordingly
+    date_str = 'April 5 - 7'
+    sheet.cell(row=1, column=2, value=event_name)
+    sheet.cell(row=3, column=2, value=date_str)
 
     for entry in series_entries:
         sheet.cell(row=current, column=1, value=series_name)
         sheet.cell(row=current, column=2, value=entry['number'])
         sheet.cell(row=current, column=3, value=entry['Team Name'])
         sheet.cell(row=current, column=4, value=entry['driver1'])
-        sheet.cell(row=current, column=5, value=entry[''])
+        sheet.cell(row=current, column=5, value=entry['NAT'])
+        sheet.cell(row=current, column=6, value=entry['sponsors'])
+        sheet.cell(row=current, column=7, value=entry['vehicle'])
+        sheet.cell(row=current, column=8, value=entry['classif'])
+
+    current += 1
