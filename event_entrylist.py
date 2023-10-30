df0f137f49f5a78ded8ce0d7e3af6d77ed84b4e7
Brendan Woo <woo.brendan@gmail.com>
2023-10-30 15:45:51 -0400

    import csv to series entries, create file name to save as, and call the handle single drivers function, passing gtam entries. save the file. create main block to run file

--- event_entrylist.py
+++ event_entrylist.py
@@ -1,10 +1,22 @@
 from functions.excel_handlers import handle_single_driver
+from functions.csv_to_series_entries import csv_to_series_entries
 import openpyxl
 
 
 def event_entrylist(series_entries):
     wb = openpyxl.load_workbook('./templates/entry_list_template.xlsx')
 
-    sheet = wb['GTAM']
+    file_name = "Sonoma Provisional Entry List"
 
     entries = series_entries['GTAM']
+
+    handle_single_driver(wb, entries)
+
+    wb.save(f'./entry_lists/{file_name}.xlsx')
+
+
+if __name__ == "__main__":
+    event = "Sonoma Raceway"
+    entries_csv = './entries_23.csv'
+    entries = csv_to_series_entries(entries_csv, event)
+    event_entrylist(entries)
