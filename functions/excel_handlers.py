d996eb8d3a48613ddca32f9873a155ee9cf476dd
Brendan Woo <woo.brendan@gmail.com>
2023-10-31 15:00:21 -0400

    remove if block for calling sortentries. add col values for sponsor vehicle and classif

--- functions/excel_handlers.py
+++ functions/excel_handlers.py
@@ -34,8 +34,7 @@ def handle_single_driver(wb, entries):
     sort_arr = ['GT3', 'GT2', 'GT4'] if series == 'GTAM' else [
         'TCX', 'TC', 'TCA']
 
-    if series == 'GTAM':
-        entries = sortEntriesByClass(entries, sort_arr)
+    entries = sortEntriesByClass(entries, sort_arr)
 
     # Entry list title, event name and date
     event_name = first_entry['event']
@@ -48,13 +47,17 @@ def handle_single_driver(wb, entries):
 
         entry['classif'] = 'SRO3' if series == 'GTAM' and entry['classif'] == 'GT3' else entry['classif']
 
+        sponsor_col = 8 if series == 'GTAM' else 6
+        vehicle_col = 9 if series == 'GTAM' else 7
+        classif_col = 10 if series == 'GTAM' else 8
+
         sheet.cell(row=current, column=1, value=series_name)
         sheet.cell(row=current, column=2, value=entry['number'])
         sheet.cell(row=current, column=3, value=entry['Team Name'])
         sheet.cell(row=current, column=4, value=entry['driver1'])
         sheet.cell(row=current, column=5, value=entry['NAT'])
-        sheet.cell(row=current, column=8, value=entry['sponsors'])
-        sheet.cell(row=current, column=9, value=entry['vehicle'])
-        sheet.cell(row=current, column=10, value=entry['classif'])
+        sheet.cell(row=current, column=sponsor_col, value=entry['sponsors'])
+        sheet.cell(row=current, column=vehicle_col, value=entry['vehicle'])
+        sheet.cell(row=current, column=classif_col, value=entry['classif'])
 
         current += 1
