77e7ef5b2ca9b16f2d01c7100495773b496321eb
Brendan Woo <woo.brendan@gmail.com>
2023-11-14 14:59:59 -0500

    replace dict creation and use a deep copy of  events_dict. then create logic to loop through all entries and add to appropriate events

--- format_entry_list/sortByEvent.py
+++ format_entry_list/sortByEvent.py
@@ -1,9 +1,17 @@
 # take  in array of entries, and sort by event in dict
-from format_entry_list import events
+from format_entry_list.events import events_dict
+import json
+import copy
 
 
 def sortByEvent(all_entries):
-    event_dict = {}
+    entry_event_dict = copy.deepcopy(events_dict)
 
-    for event in events:
-        event_dict[event] = []
+    for entry in all_entries:
+        event_name = entry['eventLabel']
+
+        entry_event_dict[event_name].append(entry)
+
+    print(json.dumps(entry_event_dict, indent=4))
+
+    return entry_event_dict
