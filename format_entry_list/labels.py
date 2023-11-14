2e317a95d51568e753ae3c2e06238978c1f75656
Brendan Woo <woo.brendan@gmail.com>
2023-11-14 14:37:33 -0500

    create entry list specific files, and create labels, a file t o fetch data from entries fetched from api

---
+++ format_entry_list/labels.py
@@ -0,0 +1,10 @@
+labels = [
+    'Car Series',  # GT4, GTWCA
+    'Championship / Class',  # pro-am
+    'Registered Car #',
+    # team name
+    'GT4 Car Make/Model'
+    # presumably othe car make
+    'Team Sponsors - Please seperate each Sponsor with a comma',
+    # primary driver
+]
