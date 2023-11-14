b0f9850bbcb8a119e427f735b73fb3783a2c84b9
Brendan Woo <woo.brendan@gmail.com>
2023-11-14 14:59:19 -0500

    create events_dict object that has eevents and array

--- format_entry_list/events.py
+++ format_entry_list/events.py
@@ -9,3 +9,16 @@ sro_events = [
     'Barber Motorsports Park',
     'Indianapolis Motor Speedway'
 ]
+
+
+events_dict = {
+    'FULL SEASON ENTRY': [],
+    'Sonoma Raceway': [],
+    'Long Beach Grand Prix': [],
+    'Sebring International Raceway': [],
+    'Circuit of the Americas': [],
+    'Virginia International Raceway': [],
+    'Road America': [],
+    'Barber Motorsports Park': [],
+    'Indianapolis Motor Speedway': []
+}
