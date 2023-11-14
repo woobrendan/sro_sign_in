55c981317259c1f16568563007c9a4e0a3be6c20
Brendan Woo <woo.brendan@gmail.com>
2023-11-14 14:37:59 -0500

    create an events file with all events

---
+++ format_entry_list/events.py
@@ -0,0 +1,11 @@
+sro_events = [
+    'FULL SEASON ENTRY',
+    'Sonoma Raceway',
+    'Long Beack Grand Prix',
+    'Sebring International Raceway',
+    'Circuit of the Americas',
+    'Virginia International Raceway',
+    'Road America',
+    'Barber Motorsports Park',
+    'Indianapolis Motor Speedway'
+]
