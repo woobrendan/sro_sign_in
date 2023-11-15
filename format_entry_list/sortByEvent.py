d769934cb72b7646836dea3a35e6a2680a12417d
Brendan Woo <woo.brendan@gmail.com>
2023-11-15 14:17:40 -0500

    import convert series and classif, and appy to new entry

--- format_entry_list/sortByEvent.py
+++ format_entry_list/sortByEvent.py
@@ -1,6 +1,7 @@
 # take  in array of entries, and sort by event in dict
+from hmac import new
 from format_entry_list.events import events_dict
-from format_entry_list.convertEntry import convertEntry
+from format_entry_list.convertEntry import convertEntry, convertSeries, convertClassif
 import json
 import copy
 
@@ -14,10 +15,14 @@ def sortByEvent(all_entries):
         # convert entry into object entry list objs
 
         new_entry = convertEntry(entry)
+
+        new_entry['Car Series'] = convertSeries(new_entry['Car Series'])
+        new_entry["Championship / Class"] = convertClassif(
+            new_entry.get("Championship / Class"))
         print('new----', json.dumps(new_entry, indent=4))
 
         entry_event_dict[event_name].append(entry)
 
-    # print(json.dumps(entry_event_dict, indent=4))
+    print(json.dumps(entry_event_dict, indent=4))
 
     return entry_event_dict
