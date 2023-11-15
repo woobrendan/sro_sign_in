4a6ba4a4318a4aabb48286a9332450f46d6f58ba
Brendan Woo <woo.brendan@gmail.com>
2023-11-15 13:24:41 -0500

    create convertEntry functin, import lable snd loop. paste in object format of USAC app object

---
+++ format_entry_list/convertEntry.py
@@ -0,0 +1,43 @@
+from format_entry_list.labels import labels
+
+
+# Take in entry object from and convert into format usable for entry list and USAC app
+def convertEntry(entry):
+    new_entry = {}
+
+    for label in labels:
+        pass
+
+
+# USAC APP
+# {
+#    fields: {
+#        driver2category: "Bronze",
+#        driver1category: "Bronze",
+#        driver1firstName: "Robert",
+#        driver1nationality: "USA",
+#        racelinkNumber: "219311",
+#        driver1drivesFirst: true,
+#        team: "STR38 Motorsports",
+#        driver1lastName: "Mau",
+#        transponderNumber: "14974222",
+#        driver2firstName: "Chris",
+#        driver2lastName: "Allen",
+#        isTeam: true,
+#        manufacturer: "BMW",
+#        driver1licenseNationality: "USA",
+#        number: "438",
+#        driver2driverId: 2,
+#        driver1driverId: 1,
+#        car: "BMW M4 GT4 (G82)",
+#        sponsors: "Cannonball Storage, OVN, Sim Seats",
+#        series: "GT4 America",
+#        driver2licenseNationality: "USA",
+#        class: "Am",
+#        driver2nationality: "USA"
+#    },
+#    updatedAt: "2023-10-04T21:43:30.243Z",
+#    id: "entries:events-Qsd47Qg251SxArN7zAEuu:nKsb2Q4V7DnXbflK6omLb",
+#    createdAt: "2023-10-04T21:09:23.397Z",
+#    eventId: "events:Qsd47Qg251SxArN7zAEuu"
+# },
