71f4953f430580e89638ea2550b53f8de6a14804
Brendan Woo <woo.brendan@gmail.com>
2023-11-16 14:46:13 -0500

    add driver 1 and 2 to labels, create car_types list

--- format_entry_list/labels.py
+++ format_entry_list/labels.py
@@ -3,9 +3,17 @@ labels = [
     'Championship / Class',  # pro-am
     'Registered Car #',
     'team',
-    'driver1Name',
-    # 'GT4 Car Make/Model'
-    # presumably othe car make
+    'driver1',
+    'driver2',
     'Team Sponsors - Please seperate each Sponsor with a comma',
-    # primary driver
+]
+
+
+car_types = [
+    'TCX Car Make / Model',
+    'TC Car Make / Model',
+    'TCA Car Make / Model',
+    "FGTWCA Car Make / Model",
+    'GTA Car Make/Model',
+    'GT4 Car Make/Model'
 ]
