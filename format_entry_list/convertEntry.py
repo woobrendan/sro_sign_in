71800184e9c939df7a05f88e96114230ebe17646
Brendan Woo <woo.brendan@gmail.com>
2023-11-16 13:38:12 -0500

    move logic for class thats not in TC to be inside second if block. change first if block to have and statement. add if block for reg car nubmer to be saved as numberr

--- format_entry_list/convertEntry.py
+++ format_entry_list/convertEntry.py
@@ -37,21 +37,25 @@ def convertEntry(entry):
     for label in labels:
         for field in entry["fieldData"]:
 
-            if label == 'Championship / Class':
-                if field["path"] == 'carType.tca':
-                    new_entry['class'] = convertClassif(field["label"])
-                    break
-                if label == field["label"]:
+            if label == 'Championship / Class' and field["path"] == 'carType.tca':
+                new_entry['class'] = convertClassif(field["label"])
+                break
+
+            if field["label"] == label:
+                if label == 'Championship / Class':
                     new_entry['class'] = convertClassif(field["value"])
                     break
 
-            if field["label"] == label:
                 if label == "Team Sponsors - Please seperate each Sponsor with a comma":
                     new_entry["sponsors"] = field["value"]
                     break
 
                 if label == 'Car Series':
                     new_entry["series"] = convertSeries(field["value"])
+                    break
+
+                if label == "Registered Car #":
+                    new_entry['number'] = field["value"]
                     break
 
                 new_entry[label] = field["value"]
