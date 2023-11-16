99b1ecbe50fa1d55ded1b4fbbca4ff732ecb1249
Brendan Woo <woo.brendan@gmail.com>
2023-11-16 14:31:35 -0500

    add label for driver1 and team

--- format_entry_list/labels.py
+++ format_entry_list/labels.py
@@ -2,7 +2,8 @@ labels = [
     'Car Series',  # GT4, GTWCA
     'Championship / Class',  # pro-am
     'Registered Car #',
-    # team name
+    'team',
+    'driver1Name',
     # 'GT4 Car Make/Model'
     # presumably othe car make
     'Team Sponsors - Please seperate each Sponsor with a comma',
