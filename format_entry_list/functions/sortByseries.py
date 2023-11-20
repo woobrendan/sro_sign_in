50ea34ba7ff06223515001f79a701463128adedb
Brendan Woo <woo.brendan@gmail.com>
2023-11-20 13:35:34 -0500

    remove duplicate series variable keying into entry val to get series. instead pass series short value to series key and to sers get

--- format_entry_list/functions/sortByseries.py
+++ format_entry_list/functions/sortByseries.py
@@ -6,12 +6,11 @@ def sortBySeries(entry_arr):
     series = {}
     for entry in entry_arr:
         series_short = getSeriesShort(entry['series'])
-        series_name = entry[series_short]
 
         #  return array of entries if key exists, else return empty arr
-        series_entries = series.get(series_name, [])
+        series_entries = series.get(series_short, [])
         series_entries.append(entry)
-        series[series_name] = series_entries
+        series[series_short] = series_entries
 
     for series_name in series.keys():
         series[series_name].sort(key=sort_by_num)
