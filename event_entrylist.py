d6a4d5a8655cba712ebe1e7e7c6813d231bab37a
Brendan Woo <woo.brendan@gmail.com>
2023-11-20 12:39:20 -0500

    copy original event entry list function, and use with new entries coming from API. make first test just GTAM

--- event_entrylist.py
+++ event_entrylist.py
@@ -1,21 +1,47 @@
-from functions.excel_handlers import handle_single_driver
+# from functions.excel_handlers import handle_single_driver
+from format_entry_list.handleSingleSeries import handle_single_driver
 from functions.csv_to_series_entries import csv_to_series_entries
 import openpyxl
 
 
+# def event_entrylist(series_entries):
+#    wb = openpyxl.load_workbook('./templates/entry_list_template.xlsx')
+#
+#    file_name = "Sonoma Provisional Entry List"
+#
+#    for series in ['GTAM', 'TCAM']:
+#        entries = series_entries[series]
+#
+#        handle_single_driver(wb, entries)
+##
+# entries = series_entries['GTAM']
+##
+# handle_single_driver(wb, entries)
+#
+#    wb.save(f'./entry_lists/{file_name}.xlsx')
+#
+#
+# if __name__ == "__main__":
+#    event = "Sonoma Raceway"
+#    entries_csv = './entries_23.csv'
+#    entries = csv_to_series_entries(entries_csv, event)
+#    event_entrylist(entries)
+
+
+# refactor using entries from API
 def event_entrylist(series_entries):
     wb = openpyxl.load_workbook('./templates/entry_list_template.xlsx')
 
     file_name = "Sonoma Provisional Entry List"
 
-    for series in ['GTAM', 'TCAM']:
-        entries = series_entries[series]
+#    for series in ['GTAM', 'TCAM']:
+#        entries = series_entries[series]
+#
+#        handle_single_driver(wb, entries)
+#
+    entries = series_entries['GTAM']
 
-        handle_single_driver(wb, entries)
-#
-#    entries = series_entries['GTAM']
-#
-#    handle_single_driver(wb, entries)
+    handle_single_driver(wb, entries)
 
     wb.save(f'./entry_lists/{file_name}.xlsx')
 
