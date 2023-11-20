490ad79f5787772797537a6d2959d2d67b8e3726
Brendan Woo <woo.brendan@gmail.com>
2023-11-20 13:02:18 -0500

    inside main block, add sequential logic to fetch data nd sort by event and test the output

--- event_entrylist.py
+++ event_entrylist.py
@@ -1,7 +1,10 @@
 # from functions.excel_handlers import handle_single_driver
+from fetch_entries import fetch_entries
 from format_entry_list.handleSingleSeries import handle_single_driver
+from format_entry_list.functions.sortByEvent import sortByEvent
 from functions.csv_to_series_entries import csv_to_series_entries
 import openpyxl
+import json
 
 
 # def event_entrylist(series_entries):
@@ -43,11 +46,15 @@ def event_entrylist(series_entries):
 
     handle_single_driver(wb, entries)
 
-    wb.save(f'./entry_lists/{file_name}.xlsx')
+    # wb.save(f'./entry_lists/{file_name}.xlsx')
 
 
 if __name__ == "__main__":
     event = "Sonoma Raceway"
-    entries_csv = './entries_23.csv'
-    entries = csv_to_series_entries(entries_csv, event)
+    data = fetch_entries()
+    sorted_entries = sortByEvent(data)
+    print('sorted_entries', json.dumps(sorted_entries, indent=4))
+    entries = sorted_entries.get(event)
+    # sort by series
+    # entries = csv_to_series_entries(entries_csv, event)
     event_entrylist(entries)
