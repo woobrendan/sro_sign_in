a5a954bc9446c23116af34f2e8b175e6f0bc406b
Brendan Woo <woo.brendan@gmail.com>
2023-11-22 15:03:32 -0500

    remove unused imports, remove prrint statement

--- format_entry_list/functions/sortByEvent.py
+++ format_entry_list/functions/sortByEvent.py
@@ -1,7 +1,6 @@
 # take  in array of entries, and sort by event in dict
 from format_entry_list.events import events_dict
-from format_entry_list.convertEntry import convertEntry, convertSeries, convertClassif
-import json
+from format_entry_list.convertEntry import convertEntry
 import copy
 
 
@@ -14,8 +13,6 @@ def sortByEvent(all_entries):
         # convert entry into object entry list objs
 
         new_entry = convertEntry(entry)
-
-        #print('new----', json.dumps(new_entry, indent=4))
 
         entry_event_dict[event_name].append(new_entry)
 
