98e726595d3a521749661d422978275ddb01d7dd
Brendan Woo <woo.brendan@gmail.com>
2023-11-24 16:29:26 -0500

    add param event to event__entrylist. pass to each handler function for series

--- event_entrylist.py
+++ event_entrylist.py
@@ -1,54 +1,27 @@
-# from functions.excel_handlers import handle_single_driver
 from fetch_entries import fetch_entries
 from format_entry_list.handleSingleSeries import handle_single_driver
 from format_entry_list.handleDualSeries import handle_dual_driver
 from format_entry_list.functions.sortByEvent import sortByEvent
 from format_entry_list.functions.sortByseries import sortBySeries
 from format_entry_list.test.test_entries import test_entries
-# from functions.csv_to_series_entries import csv_to_series_entries
 import openpyxl
-import json
-
-
-# def event_entrylist(series_entries):
-#    wb = openpyxl.load_workbook('./templates/entry_list_template.xlsx')
-#
-#    file_name = "Sonoma Provisional Entry List"
-#
-#    for series in ['GTAM', 'TCAM']:
-#        entries = series_entries[series]
-#
-#        handle_single_driver(wb, entries)
-##
-# entries = series_entries['GTAM']
-##
-# handle_single_driver(wb, entries)
-#
-#    wb.save(f'./entry_lists/{file_name}.xlsx')
-#
-#
-# if __name__ == "__main__":
-#    event = "Sonoma Raceway"
-#    entries_csv = './entries_23.csv'
-#    entries = csv_to_series_entries(entries_csv, event)
-#    event_entrylist(entries)
 
 
 # refactor using entries from API
-def event_entrylist(series_entries):
+def event_entrylist(series_entries, event):
     wb = openpyxl.load_workbook('./templates/entry_list_template.xlsx')
 
     file_name = "Sonoma Provisional Entry List"
 
-    for series in ['GTAM', 'TCAM']:
+    for series in ['GTAM', 'TCAM', 'GR Cup']:
         entries = series_entries[series]
 
-        handle_single_driver(wb, entries)
+        handle_single_driver(wb, entries, event)
 
     for series in ['GTWCA', 'PGT4A']:
         entries = series_entries[series]
 
-        handle_dual_driver(wb, entries)
+        handle_dual_driver(wb, entries, event)
 
     wb.save(f'./entry_lists/{file_name}.xlsx')
 
@@ -65,4 +38,4 @@ if __name__ == "__main__":
     entries = sorted_entries.get(event)
     series_entries = sortBySeries(entries)
 
-    event_entrylist(series_entries)
+    event_entrylist(series_entries, event)
