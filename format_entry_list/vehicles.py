ffe9dc73f4ee03be5b906472a43809f20f59623c
Brendan Woo <woo.brendan@gmail.com>
2023-11-24 16:34:25 -0500

    create dict of vehicles with camel case names and their return value in normal lettering

---
+++ format_entry_list/vehicles.py
@@ -0,0 +1,8 @@
+vehicles = {
+    'acuraNsxGt3Evo22': 'Acura NSX GT3 EVO22',
+    'astonMartinVantageAmrGt3': 'Aston Martin Vantage AMR GT3',
+    'audiR8LmsGt2': 'Audi R8 LMS GT2',
+    'bmwM4Gt3': 'BMW M4 GT3',
+    'nissanZGt4': 'Nissan Z GT4',
+
+}
