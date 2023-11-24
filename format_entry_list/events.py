04778e9a9f3e494251e8838f77e043553b5e522a
Brendan Woo <woo.brendan@gmail.com>
2023-11-24 16:46:46 -0500

    change sro events to be object, and add value as event date

--- format_entry_list/events.py
+++ format_entry_list/events.py
@@ -1,14 +1,14 @@
-sro_events = [
-    'FULL SEASON ENTRY',
-    'Sonoma Raceway',
-    'Long Beach Grand Prix',
-    'Sebring International Raceway',
-    'Circuit of the Americas',
-    'Virginia International Raceway',
-    'Road America',
-    'Barber Motorsports Park',
-    'Indianapolis Motor Speedway'
-]
+sro_events = {
+    'FULL SEASON ENTRY': "",
+    'Sonoma Raceway': "Apr 5-7",
+    'Long Beach Grand Prix': 'Apr 19-21',
+    'Sebring International Raceway': 'May 3-5',
+    'Circuit of the Americas': 'May 17-19',
+    'Virginia International Raceway': 'July 19-21',
+    'Road America': 'Aug 16-18',
+    'Barber Motorsports Park': 'Sept 6-8',
+    'Indianapolis Motor Speedway': 'Oct 4-6'
+}
 
 
 events_dict = {
