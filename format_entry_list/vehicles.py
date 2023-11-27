e8774b97f4e89ad32286618446189c567cd0e9c0
Brendan Woo <woo.brendan@gmail.com>
2023-11-27 17:04:08 -0500

    add in gt3 and gt4 cars

--- format_entry_list/vehicles.py
+++ format_entry_list/vehicles.py
@@ -3,6 +3,19 @@ vehicles = {
     'astonMartinVantageAmrGt3': 'Aston Martin Vantage AMR GT3',
     'audiR8LmsGt2': 'Audi R8 LMS GT2',
     'bmwM4Gt3': 'BMW M4 GT3',
+    'chevroletCorvetteZ06Gt3.r': 'Chevrolet Corvette Z06 GT3.R',
+    'ferrari296Gt3': 'Ferrari 296 GT3',
+    'fordMustangGt3': 'Ford Mustang GT3',
+    'mercedes-amgGt3': 'Mercedes-AMG GT3',
+    'ktmX-bowGt2': 'KTM X-Bow GT2',
     'nissanZGt4': 'Nissan Z GT4',
+    'bmwM4Gt4': 'BMW M4 GT4',
+    'fordMustangGt4': 'Ford Mustang GT4',
+    'mclarenArturaGt4': 'McLaren Artura GT4',
+    'mercedes-amgGt4': 'Mercedes-AMG GT4',
+    'porsche718CaymanGt4RsClubsport': 'Porsche 718 Cayman GT4 RS Clubsport',
+    'toyotaGazooRacingGrSupraGt4Evo': 'Toyota Gazoo Racing GR Supra GT4 EVO',
+    'acuraIntegraTypeS': 'Acura Integra Type S',
+    'subaruBrz': 'Subaru BRZ'
 
 }
