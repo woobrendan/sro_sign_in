398cde177385a331036b9ff500eef30c80aed22d
Brendan Woo <woo.brendan@gmail.com>
2023-11-29 17:22:38 -0500

    remove getserries func and move to utlity inside csv folderr

--- utility/utility.py
+++ utility/utility.py
@@ -1,11 +1,3 @@
-def getSeries(series_name):
-    series = {
-        'GT4 America': 'PGT4A',
-        'GTWCA': 'GTWCA',
-        'GTA': 'GTAM'
-    }
-    return series[series_name]
-
 
 def getSeriesShort(series_name):
     series = {
