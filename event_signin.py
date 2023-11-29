623d326514a50f8c39e216536213542847bd53aa
Brendan Woo <woo.brendan@gmail.com>
2023-11-29 18:02:17 -0500

    remove csv logic, remove csv param from event_signin, create event entries to get using key event, full season and test entries. import sort by series, designate as class entries and pass to create driver signin

--- event_signin.py
+++ event_signin.py
@@ -1,23 +1,24 @@
-from utility.csv_to_series_entries import csv_to_series_entries
 from utility.create_drivers_signin import create_drivers_signin
-from utility.create_manager_signin import create_manager_signin
+# from utility.create_manager_signin import create_manager_signin
 from fetch_entries import fetch_entries
-from
-
-# Change variables accordingly
-entries_csv = './entries_23.csv'
-event_name = 'Road America'
+from utility.sortFuncs import sortByEvent, sortBySeries
+from format_entry_list.test.test_entries import test_entries
 
 
 # Use to create all sign in sheets
 
-def event_signin(csv_file, event):
-    # entries = csv_to_series_entries(csv_file, event)
-    # create_manager_signin(entries, event)
+def event_signin(event):
     data = fetch_entries()
     sorted_entries = sortByEvent(data)
-    create_drivers_signin(sorted_entries, event)
+    event_entries = sorted_entries[event] + \
+        sorted_entries['FULL SEASON ENTRY'] + test_entries
+
+    class_entries = sortBySeries(event_entries)
+
+    # create_manager_signin(entries, event)
+    create_drivers_signin(class_entries, event)
 
 
 if __name__ == "__main__":
-    event_signin(entries_csv, event_name)
+    event_name = 'Sonoma Raceway'
+    event_signin(event_name)
