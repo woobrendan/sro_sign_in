ff5dd42760e63dad2cab1d463ba0b685e7894489
Brendan Woo <woo.brendan@gmail.com>
2023-11-30 18:20:43 -0500

    add back in logic to update manager sign in sheets

--- event_signin.py
+++ event_signin.py
@@ -1,6 +1,6 @@
-from utility.create_drivers_signin import create_drivers_signin
-# from utility.create_manager_signin import create_manager_signin
-from fetch_entries import fetch_entries
+from format_sign_in.create_drivers_signin import create_drivers_signin
+from format_sign_in.create_manager_signin import create_manager_signin
+from utility.fetch_entries import fetch_entries
 from utility.sortFuncs import sortByEvent, sortBySeries
 from format_entry_list.test.test_entries import test_entries
 
@@ -15,7 +15,7 @@ def event_signin(event):
 
     class_entries = sortBySeries(event_entries)
 
-    # create_manager_signin(entries, event)
+    create_manager_signin(class_entries, event)
     create_drivers_signin(class_entries, event)
 
 
