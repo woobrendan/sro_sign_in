901bfbc52ad20501a0547f9bce3a018a750f23db
Brendan Woo <woo.brendan@gmail.com>
2023-12-01 14:46:20 -0500

    remove vehicles import, change car value to be entry.car and remove vehicles.get(car) variable

--- format_entry_list/handleSingleSeries.py
+++ format_entry_list/handleSingleSeries.py
@@ -1,7 +1,6 @@
 from utility.sortFuncs import sortEntriesByClass
 from utility.utility import getSeriesShort
 from utility.events import sro_events
-from format_entry_list.vehicles import vehicles
 
 
 def handle_single_driver(wb, entries, event):
@@ -22,7 +21,6 @@ def handle_single_driver(wb, entries, event):
 
     for entry in sorted_entries:
         car_num = entry["number"]
-        car = vehicles.get(entry['car'], entry['car'])
 
         if not car_num.startswith('0') and car_num.isdigit():
             car_num = int(car_num)
@@ -42,7 +40,7 @@ def handle_single_driver(wb, entries, event):
             sheet.cell(row=current, column=7, value='Bronze')
 
         sheet.cell(row=current, column=sponsor_col, value=entry['sponsors'])
-        sheet.cell(row=current, column=vehicle_col, value=car)
+        sheet.cell(row=current, column=vehicle_col, value=entry['car'])
         sheet.cell(row=current, column=classif_col, value=entry['class'])
 
         current += 1
