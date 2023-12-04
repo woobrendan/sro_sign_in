672d96c722b209821abaaca1c70bc29d8e494127
Brendan Woo <woo.brendan@gmail.com>
2023-12-04 15:19:49 -0500

    add test for gtwc and add custom message if fails

--- tests/test_entry/test_entry.py
+++ tests/test_entry/test_entry.py
@@ -1,11 +1,21 @@
 from utility.convertEntry import convertEntry
-from tests.test_entry.api_mock import gt4_entry, gt4_expected
+from tests.test_entry.api_mock import gt4_api, gt4_expected, gt3_api, gt3_expected
 
 
+# Test GT4
 def test_convert_entry():
-    api_received = gt4_entry
+    api_received = gt4_api
     expected = gt4_expected
     converted = convertEntry(api_received)
 
     assert set(converted.keys()) == set(expected.keys())
     assert set(converted.values()) == set(expected.values())
+
+
+# Test GTWC
+def test_convert_entry():
+    api_received = gt3_api
+    expected = gt3_expected
+    converted = convertEntry(api_received)
+
+    assert expected == converted, "Expected convert Entry function to take in GTWC api entry and convert it into easy to read dict"
