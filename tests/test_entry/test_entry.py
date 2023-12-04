0c8912eb1171cd88bf64f1257cf69d6797e4445b
Brendan Woo <woo.brendan@gmail.com>
2023-12-04 15:26:58 -0500

    add in test for gtam and tcx. change all assertions to check keys and values to give more clear errors

--- tests/test_entry/test_entry.py
+++ tests/test_entry/test_entry.py
@@ -1,5 +1,5 @@
 from utility.convertEntry import convertEntry
-from tests.test_entry.api_mock import gt4_api, gt4_expected, gt3_api, gt3_expected
+from tests.test_entry.api_mock import gt4_api, gt4_expected, gt3_api, gt3_expected, gtam_api, gtam_expected, tcx_api, tcx_expected
 
 
 # Test GT4
@@ -18,4 +18,28 @@ def test_convert_entry():
     expected = gt3_expected
     converted = convertEntry(api_received)
 
-    assert expected == converted, "Expected convert Entry function to take in GTWC api entry and convert it into easy to read dict"
+    assert set(converted.keys()) == set(expected.keys())
+    assert set(converted.values()) == set(expected.values(
+    )), "Expected convert Entry function to take in GTWC api entry and convert it into easy to read dict"
+
+
+# Test GTAM
+def test_convert_entry():
+    api_received = gtam_api
+    expected = gtam_expected
+    converted = convertEntry(api_received)
+
+    assert set(converted.keys()) == set(expected.keys())
+    assert set(converted.values()) == set(expected.values(
+    )), "Expected convert Entry function to take in GTAM api entry and convert it into easy to read dict"
+
+
+# Test TCX
+def test_convert_entry():
+    api_received = tcx_api
+    expected = tcx_expected
+    converted = convertEntry(api_received)
+
+    assert set(converted.keys()) == set(expected.keys())
+    assert set(converted.values()) == set(expected.values(
+    )), "Expected convert Entry function to take in TCX api entry and convert it into easy to read dict"
