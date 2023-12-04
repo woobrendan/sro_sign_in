f0dc3bf15d856530b314f36dd5e4a92163a2fb52
Brendan Woo <woo.brendan@gmail.com>
2023-12-04 14:49:15 -0500

    assign converted value to convertentry func and assert keys and values to assert equality

--- tests/test_entry/test_entry.py
+++ tests/test_entry/test_entry.py
@@ -1,10 +1,11 @@
-from lib2to3.pytree import convert
 from utility.convertEntry import convertEntry
-from .api_mock import gt4_entry, gt4_expected
+from tests.test_entry.api_mock import gt4_entry, gt4_expected
 
 
 def test_convert_entry():
     api_received = gt4_entry
     expected = gt4_expected
+    converted = convertEntry(api_received)
 
-    assert convertEntry(api_received) == expected
+    assert set(converted.keys()) == set(expected.keys())
+    assert set(converted.values()) == set(expected.values())
