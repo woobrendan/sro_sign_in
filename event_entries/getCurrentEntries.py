0fd1025309b46f0fb2773439cdb2098ee35d5f53
Brendan Woo <woo.brendan@gmail.com>
2023-12-05 16:48:03 -0500

    add created value to headers. update import path for master entry list. create iteration of each row to return dict and have result entries

--- event_entries/getCurrentEntries.py
+++ event_entries/getCurrentEntries.py
@@ -2,10 +2,16 @@ import pandas as pd
 
 
 headers = ['event', 'series', 'class', 'number', 'team', 'driver1firstName', 'driver1lastName', 'driver1nationality', 'driver1category',
-           'driver2firstName', 'driver2lastName', 'driver2nationality', 'driver2category', 'sponsors', 'car', 'manufacturer', 'id']
+           'driver2firstName', 'driver2lastName', 'driver2nationality', 'driver2category', 'sponsors', 'car', 'manufacturer', 'id', 'created']
 
 
 def getCurrentEntries():
-    data = pd.read_excel('./master_entry_list.xlsx')
+    data = pd.read_excel('./event_entries/master_entry_list.xlsx')
 
     results = []
+
+    for i, row in data.iterrows():
+        row_dict = {f'{headers[i]}': value for i, value in enumerate(row)}
+        results.append(row_dict)
+
+    return results
