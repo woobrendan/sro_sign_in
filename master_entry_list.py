ab34a827306fcdb028f1f6c8cbf72d946151d8b2
Brendan Woo <woo.brendan@gmail.com>
2023-12-06 17:04:14 -0500

    add var for existing ids, using for loop to return array of ids. add in filterentiresbyid func and pass filtered entries to writetoMaster

--- master_entry_list.py
+++ master_entry_list.py
@@ -2,6 +2,7 @@ import json
 from event_entries.writeToMasterEntry import writeToMasterEntry
 from event_entries.getCurrentEntries import getCurrentEntries
 from utility.fetch_entries import fetch_entries
+from utility.utility import filterEntriesById
 import openpyxl
 
 
@@ -12,18 +13,22 @@ def master_entry_list():
 
 # take each row and convert into dict
     existing_entries = getCurrentEntries()
+    existing_ids = [entry["id"] for entry in existing_entries]
+
 # fetch responses
     api_entries = fetch_entries()
-# filter for unique ids
 
+# filter for unique ids
+    filtered_entries = filterEntriesById(existing_ids, api_entries)
 
 # add those new entries to entry lists
-    # writeToMasterEntry(entries, sheet)
+    writeToMasterEntry(filtered_entries, sheet)
 
 
 # print out x new entries added
 
 
 if __name__ == "__main__":
-    entries = getCurrentEntries()
-    print(json.dumps(entries, indent=4))
+    # entries = getCurrentEntries()
+    # print(json.dumps(entries, indent=4))
+    master_entry_list()
