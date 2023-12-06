e60e79740d8145fc601815b22a82eee5ac98bfd1
Brendan Woo <woo.brendan@gmail.com>
2023-12-06 17:38:35 -0500

    remove headers variable and move to its own file, import that array and use

--- event_entries/getCurrentEntries.py
+++ event_entries/getCurrentEntries.py
@@ -1,8 +1,5 @@
 import pandas as pd
-
-
-headers = ['event', 'series', 'class', 'number', 'team', 'driver1firstName', 'driver1lastName', 'driver1nationality', 'driver1category',
-           'driver2firstName', 'driver2lastName', 'driver2nationality', 'driver2category', 'sponsors', 'car', 'manufacturer', 'id', 'created']
+from event_entries.headers import headers
 
 
 def getCurrentEntries():
