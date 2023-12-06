ccf8a16bad9d009930c5f83ea4d97ffa17ee2243
Brendan Woo <woo.brendan@gmail.com>
2023-12-06 17:37:42 -0500

    transfer over headers array for master entry list headers

---
+++ event_entries/headers.py
@@ -0,0 +1,2 @@
+headers = ['event', 'series', 'class', 'number', 'team', 'driver1firstName', 'driver1lastName', 'driver1nationality', 'driver1category',
+           'driver2firstName', 'driver2lastName', 'driver2nationality', 'driver2category', 'sponsors', 'car', 'manufacturer', 'id', 'created']
