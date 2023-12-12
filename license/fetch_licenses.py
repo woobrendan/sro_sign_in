919a025b1bb95b02aa63511060ced63ca0c8104d
Brendan Woo <woo.brendan@gmail.com>
2023-12-12 12:40:57 -0800

    create folder fo licensing, and ceagte fetch license file

---
+++ license/fetch_licenses.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from utility.sortFuncs import sortByEvent
+import requests
+import os
+
+load_dotenv()
+
+
+def fetch_entries():
+
+    token = os.environ.get('TKSPICE')
+    form = os.environ.get('FORM_ID')
+
+    try:
+        response = requests.get(
+            url="https://api.webconnex.com/v2/public/search/registrants",
+            params={
+                "product": "redpodium.com2",
+                "formId": form
+            },
+            headers={
+                'apiKey': token
+            }
+        )
+        if response.status_code == 200:
+            data = response.json()
+
+            return data['data']
+
+    except requests.exceptions.RequestException:
+        print(f'HTTP Request failed error {response.status_code}')
+
+
+if __name__ == '__main__':
+    entries = fetch_entries()
+    sortByEvent(entries)
