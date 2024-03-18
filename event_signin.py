5de2a11674f069640ae917c347d5730eaee9f457
Brendan Woo <woo.brendan@gmail.com>
2024-03-18 17:51:49 -0400

    import fetchevent entries, and pass event. import convertentry and convert each entry

--- event_signin.py
+++ event_signin.py
@@ -1,19 +1,18 @@
 from format_sign_in.create_drivers_signin import create_drivers_signin
 from format_sign_in.create_manager_signin import create_manager_signin
-from utility.fetch_entries import fetch_entries
-from utility.sortFuncs import sortByEvent, sortBySeries
-from format_entry_list.test.test_entries import test_entries
+from utility.fetch_event_entries import fetch_event_entries
+from utility.sortFuncs import sortBySeries
+from utility.convertEntry import convertEntry
 
 
 # Use to create all sign in sheets
 
 def event_signin(event):
-    data = fetch_entries()
-    sorted_entries = sortByEvent(data)
-    event_entries = sorted_entries[event] + \
-        sorted_entries['FULL SEASON ENTRY'] + test_entries
+    data = fetch_event_entries(event)
+    
+    converted = [convertEntry(entry) for entry in data]
 
-    class_entries = sortBySeries(event_entries)
+    class_entries = sortBySeries(converted)
 
     create_manager_signin(class_entries, event)
     create_drivers_signin(class_entries, event)
