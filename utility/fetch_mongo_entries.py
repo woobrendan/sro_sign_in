35acd6dff3e415e3b2016eb9a4d0d9e0a6335b1c
Brendan Woo <woo.brendan@gmail.com>
2024-04-12 17:08:38 -0700

    fix event data key value to get from entry instead of data

--- utility/fetch_mongo_entries.py
+++ utility/fetch_mongo_entries.py
@@ -1,5 +1,6 @@
 import requests
 
+
 # turn on mongo express server from podium assistant to update entries
 
 def fetch_all_entries():
@@ -8,6 +9,17 @@ def fetch_all_entries():
     if response.status_code == 201:
         data = response.json()
          
-        return data['data']
+        return data['entry']
+    else:
+        print(f"Error fetching from own server: {response.status_code}")
+
+
+def fetch_event_entries(event):
+    response = requests.get('http://localhost:2020/api/entries')
+
+    if response.status_code == 201:
+        data = response.json()
+         
+        return data['entry']
     else:
         print(f"Error fetching from own server: {response.status_code}")
