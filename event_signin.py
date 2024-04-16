ff5d08e303824e020ba2535baa150377d31c2130
Brendan Woo <woo.brendan@gmail.com>
2024-04-15 17:18:04 -0700

    comment out old fetch event entries import, and comment out converted logic, instead pass data directly

--- event_signin.py
+++ event_signin.py
@@ -1,8 +1,9 @@
 from format_sign_in.create_drivers_signin import create_drivers_signin
 from format_sign_in.create_manager_signin import create_manager_signin
-from utility.fetch_event_entries import fetch_event_entries
+# from utility.fetch_event_entries import fetch_event_entries
 from utility.sortFuncs import sortBySeries
 from utility.convertEntry import convertEntry
+from utility.fetch_mongo_entries import fetch_all_entries, fetch_event_entries
 
 
 # Use to create all sign in sheets
@@ -10,9 +11,10 @@ from utility.convertEntry import convertEntry
 def event_signin(event):
     data = fetch_event_entries(event)
     
-    converted = [convertEntry(entry) for entry in data]
+    # converted = [convertEntry(entry) for entry in data]
 
-    class_entries = sortBySeries(converted)
+    # class_entries = sortBySeries(converted)
+    class_entries = sortBySeries(data)
 
     create_manager_signin(class_entries, event)
     create_drivers_signin(class_entries, event)
