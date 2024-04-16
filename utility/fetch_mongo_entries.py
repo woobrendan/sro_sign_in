8e88e0be6334363f801fd0fd06bf8dd6ce68dc7d
Brendan Woo <woo.brendan@gmail.com>
2024-04-15 17:13:12 -0700

    create event fetch handler to take in param of event

--- utility/fetch_mongo_entries.py
+++ utility/fetch_mongo_entries.py
@@ -15,11 +15,11 @@ def fetch_all_entries():
 
 
 def fetch_event_entries(event):
-    response = requests.get('http://localhost:2020/api/entries')
+    response = requests.get(f'http://localhost:2020/api/entries/events/{event}')
 
-    if response.status_code == 201:
+    if response.status_code == 200:
         data = response.json()
          
-        return data['entry']
+        return data['entries']
     else:
         print(f"Error fetching from own server: {response.status_code}")
