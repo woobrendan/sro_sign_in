e473950270d92c31ee33c2a730004420ccb77e0b
Brendan Woo <woo.brendan@gmail.com>
2024-04-15 17:35:29 -0700

    add sponors variable and use entry.get to either get or return empty, pass sponors val to column

--- format_entry_list/handleSingleSeries.py
+++ format_entry_list/handleSingleSeries.py
@@ -21,6 +21,7 @@ def handle_single_driver(wb, entries, event):
 
     for entry in sorted_entries:
         car_num = entry["number"]
+        sponsors = entry.get('sponsors', "")
 
         if not car_num.startswith('0') and car_num.isdigit():
             car_num = int(car_num)
@@ -39,7 +40,7 @@ def handle_single_driver(wb, entries, event):
         if series_short == 'GTAM':
             sheet.cell(row=current, column=7, value='Bronze')
 
-        sheet.cell(row=current, column=sponsor_col, value=entry['sponsors'])
+        sheet.cell(row=current, column=sponsor_col, value=sponsors)
         sheet.cell(row=current, column=vehicle_col, value=entry['car'])
         sheet.cell(row=current, column=classif_col, value=entry['class'])
 
