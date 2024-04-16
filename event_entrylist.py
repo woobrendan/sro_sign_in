18118b1bc84c70910a748cfdee104160d5de9d01
Brendan Woo <woo.brendan@gmail.com>
2024-04-15 17:27:51 -0700

    change logic in function to use fetch from mongo server instead of webconnex

--- event_entrylist.py
+++ event_entrylist.py
@@ -4,13 +4,16 @@ from format_entry_list.handleDualSeries import handle_dual_driver
 from utility.sortFuncs import sortByEvent, sortBySeries
 from format_entry_list.test.test_entries import test_entries
 import openpyxl
+from utility.fetch_mongo_entries import fetch_event_entries
 import json
 
 
-def event_entrylist(series_entries, event):
+def event_entrylist(event):
     wb = openpyxl.load_workbook('./templates/entry_list_template.xlsx')
 
-    file_name = "Sebring Provisional Entry List"
+    series_entries = fetch_event_entries(event)
+
+    file_name = f"{event} Provisional Entry List"
 
     for series in ['GTAM', 'TCAM', 'GR Cup']:
         entries = series_entries[series]
@@ -26,12 +29,13 @@ def event_entrylist(series_entries, event):
 
 if __name__ == "__main__":
     event = "Sebring International Raceway"
-    data = fetch_entries()
-    sorted_entries = sortByEvent(data)
+    # data = fetch_entries()
+    # sorted_entries = sortByEvent(data)
 
-    sorted_entries[event] += sorted_entries['FULL SEASON ENTRY']
+    # sorted_entries[event] += sorted_entries['FULL SEASON ENTRY']
 
-    entries = sorted_entries.get(event)
-    series_entries = sortBySeries(entries)
+    # entries = sorted_entries.get(event)
+    # series_entries = sortBySeries(entries)
 
-    event_entrylist(series_entries, event)
+    # event_entrylist(series_entries, event)
+    event_entrylist(event)
