b3d28a27916ee9dad4f9675c3571204d1b8f5563
Brendan Woo <woo.brendan@gmail.com>
2024-04-24 15:24:14 -0700

    define license function, open wb and fetch entries

--- license.py
+++ license.py
@@ -1,8 +1,19 @@
 from licensing.fetch_licenses import fetch_licenses
+from licensing import handleNewLicenses
 import json
+import openpyxl
+
+def license():
+    wb = openpyxl.load_workbook('./licensing/2024_Licenses.xlsx')
+    entries = fetch_licenses()
+    print(json.dumps(entries, indent=4))
+
+    # handleNewLicenses(wb, entries)
+
+
+
 
 if __name__ == '__main__':
-    data = fetch_licenses()
-    print(json.dumps(data, indent=4))
+    license()
 
 
