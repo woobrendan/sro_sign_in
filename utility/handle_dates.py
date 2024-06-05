0737b757f19a0157d1643eb207eb85b1af1b06d3
Brendan Woo <woo.brendan@gmail.com>
2024-06-05 13:09:07 -0400

    add in values for parsed and time zoned, then return recent which is maxed value

--- utility/handle_dates.py
+++ utility/handle_dates.py
@@ -3,6 +3,8 @@ import pytz
 
 # used to parse two diff types of dates '2024-05-29T19:55:25Z' or '2024-05-19  12:42:00 PM'
 def parse_date(date_str):
+    if isinstance(date_str, datetime):
+        return date_str
     try:
          # Try to parse the date in '2024-05-19  12:42:00 PM' format
          date = datetime.strptime(date_str, "%Y-%m-%d %I:%M:%S %p")
@@ -10,7 +12,8 @@ def parse_date(date_str):
         try:
             # Try to parse the date in '2024-05-29T19:55:25Z' format
             date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
-            date = date.replace(tzinfo=pytz.UTC)  # Ensure the second format is in UTC
+            # Ensure the date is formatted with time zone
+            date = date.replace(tzinfo=pytz.UTC)
         except ValueError:
             raise ValueError(f"Date format for '{date_str}' is not recognized")
     return date
@@ -26,9 +29,10 @@ def getMostRecentDate(sheet, column_id):
         else:
             break
 
-    if submit_dates:
-        latest_date = submit_dates[-1]
-        return latest_date
-    else:
-        return date.today()
+    parsed = [parse_date(date_str) for date_str in submit_dates]
+    tzoned = [date.astimezone(pytz.UTC) for date in parsed]
+    recent = max(tzoned).strftime("%Y-%m-%dT%H:%M:%SZ")
+    print('recent', recent)
+
+    return recent if recent else datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
     
