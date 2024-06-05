5e40311cc5c84b58bd61574d7a6b06720bcc6cdf
Brendan Woo <woo.brendan@gmail.com>
2024-06-05 13:26:45 -0400

    udpate if isinstance to handle setting timze zone aware dates, update try blocks to handle time zone awareness. update return value

--- utility/handle_dates.py
+++ utility/handle_dates.py
@@ -4,15 +4,17 @@ import pytz
 # used to parse two diff types of dates '2024-05-29T19:55:25Z' or '2024-05-19  12:42:00 PM'
 def parse_date(date_str):
     if isinstance(date_str, datetime):
+        if date_str.tzinfo is None:
+            date_str = date_str.replace(tzinfo=pytz.UTC)
         return date_str
     try:
-         # Try to parse the date in '2024-05-19  12:42:00 PM' format
-         date = datetime.strptime(date_str, "%Y-%m-%d %I:%M:%S %p")
+        # Try to parse the date in '2024-05-19 12:43' format
+        date = datetime.strptime(date_str, "%Y-%m-%d %H:%M")
+        date = date.replace(tzinfo=pytz.UTC)
     except ValueError:
         try:
-            # Try to parse the date in '2024-05-29T19:55:25Z' format
+            # Try to parse the date in '2024-05-19T12:57:00Z' format
             date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
-            # Ensure the date is formatted with time zone
             date = date.replace(tzinfo=pytz.UTC)
         except ValueError:
             raise ValueError(f"Date format for '{date_str}' is not recognized")
@@ -30,9 +32,14 @@ def getMostRecentDate(sheet, column_id):
             break
 
     parsed = [parse_date(date_str) for date_str in submit_dates]
-    tzoned = [date.astimezone(pytz.UTC) for date in parsed]
-    recent = max(tzoned).strftime("%Y-%m-%dT%H:%M:%SZ")
-    print('recent', recent)
 
-    return recent if recent else datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
+    # Find the most recent datetime
+    recent = max(parsed, default=None)
+
+    if recent is not None:
+        recent = recent.strftime("%Y-%m-%dT%H:%M:%SZ")
+    else:
+        recent = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    
+    return recent
     
