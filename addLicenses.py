1290248d3157cdff7d1820362c3d753a3e1019a8
Brendan Woo <woo.brendan@gmail.com>
2024-06-05 13:35:48 -0400

    add logic to handle the sorting of new regs by date

--- addLicenses.py
+++ addLicenses.py
@@ -2,6 +2,7 @@ from utility.utility import getAllId, filterEntriesById, addValuesToExcel
 from utility.handle_dates import getMostRecentDate
 from licensing.fetch_licenses import fetch_licenses
 import openpyxl
+import json
 
 def handleNewLicenses():
 
@@ -14,8 +15,10 @@ def handleNewLicenses():
     registrations = fetch_licenses(recent_date)
 
     filtered_reg = filterEntriesById(all_ids, registrations)
-
-    count = addValuesToExcel(filtered_reg, sheet)
+    # order new registrants by date
+    new_regs = sorted(filtered_reg, key=lambda x: x['created'])
+    
+    count = addValuesToExcel(new_regs, sheet)
 
     print(f'{count} registrations have been added to the licensing document')
 
